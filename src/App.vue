bc644ba2305fc3e0f8f2fc6d1bf00ca100ffab32
julia ferenczi <julia.ferenczi@lexisnexis.co.uk>
2019-02-14 13:37:23 +0000

    HomePage Component and style added [2.section/5.part]

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <main>
+      <HomePage />
+    </main>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'app',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  min-height: 300px;
+}
+</style>
